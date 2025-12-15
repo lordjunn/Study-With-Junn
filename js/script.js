3f3461f0a9d8cf9637bed5e7b92210789e36c36e
Lord Junn <58977552+LordJunn@users.noreply.github.com>
2025-12-15 17:36:23 +0800

    Add exam images and update TDS 3301 content
    Added Theory and Formula images to TDS 3301 Exams page and updated the update log to reflect these changes. Removed the timer for the TDS 3301 midterm test from index.html and script.js as the test period has ended. Updated the last updated date in the footer.
    [Copilot above]
    
    I don wanna do this anymore
    Let's get this over with

--- js/script.js
+++ js/script.js
@@ -138,14 +138,12 @@ const countStartDates = [
     new Date("3 Nov, 2025 08:00:00").getTime(), 
     new Date("3 Nov, 2025 08:00:00").getTime(), 
     new Date("3 Nov, 2025 08:00:00").getTime(), 
-    new Date("3 Nov, 2025 08:00:00").getTime(), 
 ];
 
 const countDownDates = [
     new Date("21 Jan, 2026 18:00:00").getTime(),
     new Date("20 Feb, 2026 08:00:00").getTime(),
     new Date("9 Mar, 2026 08:00:00").getTime(),
-    new Date("15 Dec, 2025 20:15:00").getTime(), 
     new Date("3 Feb, 2026 14:00:00").getTime(), 
 ];
 
@@ -154,7 +152,6 @@ const containerIds = [
     "countdown-container-2",
     "countdown-container-3",
     "countdown-container-4",
-    "countdown-container-5",
 ];
 
 initializeCountdowns(countDownDates, countStartDates, containerIds);
